9c369aa02836740d2844d037721e2fba6d573328
Joshua Colvin <jcolvin@offchainlabs.com>
2026-05-05 18:08:23 -0700

    Set max arbos supported to ArbOS60

--- params/config_arbitrum.go
+++ params/config_arbitrum.go
@@ -47,7 +47,7 @@ const ArbosVersion_FixRedeemGas = ArbosVersion_11
 const ArbosVersion_Stylus = ArbosVersion_30
 const ArbosVersion_StylusFixes = ArbosVersion_31
 const ArbosVersion_StylusChargingFixes = ArbosVersion_32
-const MaxArbosVersionSupported = ArbosVersion_59
+const MaxArbosVersionSupported = ArbosVersion_60
 const MaxDebugArbosVersionSupported = ArbosVersion_60
 const ArbosVersion_Dia = ArbosVersion_50
 
